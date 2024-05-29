ed4d90ee7600875a8f7801bd7a91b3c02fc26b36
HLong <hailongkc1203@gmail.com>
2024-05-29 21:56:58 +0700

    adding the search functionality

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -7,15 +7,19 @@ import { MatCardModule } from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { CartService } from '../../cart/cart.service';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [NgFor, MatButtonModule, MatCardModule, FlexLayoutModule, MatSnackBarModule],
+  imports: [NgFor, MatButtonModule, MatCardModule, FlexLayoutModule, MatSnackBarModule, MatInputModule, MatFormFieldModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
+  filteredProducts: Product[] = [];
   constructor(
     private productService: ProductService,
     private cartService: CartService,
@@ -24,6 +28,7 @@ export class ProductListComponent implements OnInit {
   ngOnInit(): void {
     this.productService.getProducts().subscribe(data => {
       this.products = data;
+      this.filteredProducts = data;
     })
   }
   addToCart(product: Product): void {
@@ -36,6 +41,13 @@ export class ProductListComponent implements OnInit {
         })
       }
     });
+  }
+
+  applyFilter(event: Event): void {
+    let searchItem = (event.target as HTMLInputElement).value;;
+    searchItem = searchItem.toLowerCase();
+
+    this.filteredProducts = this.products.filter(product => product.name.toLowerCase().includes(searchItem))
   }
 
 }
