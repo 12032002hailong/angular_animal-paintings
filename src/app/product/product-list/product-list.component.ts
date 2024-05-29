8af20c774224bab3321d4747fa940bd89639f228
HLong <hailongkc1203@gmail.com>
2024-05-29 09:59:33 +0700

    creating marcards for products

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -2,11 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductService } from '../product.service';
 import { NgFor } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, MatButtonModule, MatCardModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
