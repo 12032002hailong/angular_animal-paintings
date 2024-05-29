d5d784b690e678467637a508df2259dfbd5f16fd
HLong <hailongkc1203@gmail.com>
2024-05-29 10:24:39 +0700

    applying the angular material style

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideHttpClient(), provideAnimationsAsync()]
 };
