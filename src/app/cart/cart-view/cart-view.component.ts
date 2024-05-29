fdaa0877b4eb480bd0d8ebc214af3460a96b902e
HLong <hailongkc1203@gmail.com>
2024-05-29 21:17:57 +0700

    calculating the totalPrice

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product';
+import { CartService } from '../cart.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-cart-view',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatCardModule, MatListModule, NgFor],
   templateUrl: './cart-view.component.html',
   styleUrl: './cart-view.component.css'
 })
-export class CartViewComponent {
+export class CartViewComponent implements OnInit {
+  cartItems: Product[] = [];
+  totalPrice: number = 0;
+  constructor(private cartService: CartService) { }
 
+  ngOnInit(): void {
+    this.cartService.getCartItems().subscribe(data => {
+      this.cartItems = data
+      this.totalPrice = this.getTotalPrice();
+    })
+  }
+
+  getTotalPrice(): number {
+    let total = 0;
+    this.cartItems.map(
+      item => {
+        return (
+          total += item.price
+        )
+      }
+    )
+    console.log(total);
+    return total
+  }
 }
